da89666bf70950dcaae1503cf226ccef6755b337
TotallyNotMalicious <LCMLegacy12@protonmail.com>
2021-01-26 19:20:00 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Description
+randomize the order of letters in a given word
+
+# How To Build
+```
+g++ -o main main.cpp -std=c++11
+```
+
+# How To Run
+```
+./main (on linux)
+main   (on windows, or just run the executable)
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+string randomize(const string &input)
+{
+	vector<size_t> indexes;
+
+	for (size_t i = 0; i < input.size(); i++)
+		indexes.push_back(i);
+        random_shuffle(indexes.begin(), indexes.end());
+	    string final;
+
+	for (size_t i = 0; i < indexes.size(); i++)
+		final += input[indexes[i]];
+	    return final;
+}
+
+int main()
+{
+    string input;
+    srand(size_t(time(NULL)));
+
+    cout << "Input: ";
+    cin >> input;
+	cout << "Randomized: " << randomize(input) << "\n";
+
+	return 0;
+}
